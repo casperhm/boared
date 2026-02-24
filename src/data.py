539e848e86ea83b3d4e8d54c9f79e90b023745f2
casperhm <casper@msqr.us>
2026-02-24 19:30:19 +1300

    moved parts of list setup into data.py - get_climb_list and changed the SQL query into a JOIN rather than two seperate querys

--- src/data.py
+++ src/data.py
@@ -1,21 +1,61 @@
 import sqlite3
+from kivymd.uix.list import IRightBodyTouch, MDList, ThreeLineRightIconListItem
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
 
 
-def getClimbList(db_path):
+# Just TB2 40 degrees data for now, currently capped at one 50 item page
+def get_climb_list(db_path, filters):
+    # Get the data from database file
     try:
         with sqlite3.connect(db_path) as connection:
             cursor = connection.cursor()
-            cursor.execute("SELECT name, setter_username FROM climbs")
-            climb_data0 = cursor.fetchall()
             cursor.execute(
-                "SELECT ascensionist_count, display_difficulty FROM climb_stats"
+                """
+                SELECT 
+                    c.name,
+                    c.setter_username,
+                    c.uuid,
+                    s.ascensionist_count,
+                    s.display_difficulty
+                FROM climbs c
+                JOIN climb_stats s
+                    ON c.uuid = s.climb_uuid
+                WHERE c.layout_id = ?
+                  AND c.angle = ?
+                  AND s.angle = ?
+                LIMIT 50
+            """,
+                ("11", "40", "40"),
             )
-            climb_data1 = cursor.fetchall()
-            # Combine the two sets o1f data into a single list of tuples
-            climb_data = []
-            for i, j in zip(climb_data0, climb_data1):
-                climb_data.append(i + j)
 
-            return climb_data
+            climb_data = cursor.fetchall()
+
     except sqlite3.Error as e:
         print(f"An error occurred while connecting to the database: {e}")
+
+    climb_list = MDList()
+
+    # Populate the climb list with climbs
+    for climb in climb_data:
+        item = ThreeLineRightIconListItem(
+            text=climb[0],  # Climb name
+            secondary_text=f"Set by {climb[1]}",  # Setter name
+            tertiary_text=f"{climb[3]} repeats",  # Repeat count
+        )
+        # Add the custom right container to each list item
+        item.add_widget(GradeContainer().getGrade(climb[4]))  # Climb grade
+        climb_list.add_widget(item)
+
+    return climb_list
+
+
+# Custom container for the right side of each climb list item, which includes the climb's grade and a placeholder for more info icons (has instagram beta, benchmark/classic)
+class GradeContainer(IRightBodyTouch, MDBoxLayout):
+    def getGrade(self, grade):
+        self.orientation = "vertical"
+        self.size_hint_x = None
+
+        self.add_widget(MDLabel(text=str(grade), halign="left"))
+
+        return self
--- src/pages.py
+++ src/pages.py
@@ -1,9 +1,6 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivymd.uix.list import IRightBodyTouch, MDList, ThreeLineRightIconListItem
 from kivy.uix.scrollview import ScrollView
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.label import MDLabel
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 
@@ -28,24 +25,9 @@ class HomeLayout(BoxLayout):
 
         # Create list of climbs
         scroll = ScrollView()
-        climb_list = MDList()
-
-        # Populate the climb list with climbs
-        for i in range(20):
-            # Get climb data from the database
-            climb_data = data.getClimbList("./data/tensiondata")
-
-            item = ThreeLineRightIconListItem(
-                text=climb_data[i][0],  # Climb name
-                secondary_text=f"Set by {climb_data[i][1]}",  # Setter name
-                tertiary_text=f"{climb_data[i][2]} repeats",  # Repeat count
-            )
-            # Add the custom right container to each list item
-            item.add_widget(GradeContainer().getGrade(climb_data[i][3]))  # Climb grade
-            climb_list.add_widget(item)
 
         # Add the climb list to the scroll view and then add the scroll view to the layout
-        scroll.add_widget(climb_list)
+        scroll.add_widget(data.get_climb_list("./data/tensiondata", None))
         self.add_widget(scroll)
 
 
@@ -71,14 +53,3 @@ class FilterPage(StackLayout):
                 text="Filter options will go here", size_hint=(0.5, None), height=150
             )
         )
-
-
-# Custom container for the right side of each climb list item, which includes the climb's grade and a placeholder for more info icons (has instagram beta, benchmark/classic)
-class GradeContainer(IRightBodyTouch, MDBoxLayout):
-    def getGrade(self, grade):
-        self.orientation = "vertical"
-        self.size_hint_x = None
-
-        self.add_widget(MDLabel(text=str(grade), halign="left"))
-
-        return self
